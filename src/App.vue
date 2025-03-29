4ac015e1b7e59d590620f6049f3ba680aede1119
Anderson Sánchez <anderson2002y@gmail.com>
2025-03-29 17:47:35 -0600

    feat(expenses): add button for new expenses

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
   import Presupuesto from './components/Presupuesto.vue';
   import ControlPresupuesto from './components/ControlPresupuesto.vue';
 
+  import iconoNuevogasto from './assets/img/nuevo-gasto.svg'
+
   const presupuesto = ref(0);
   const disponible = ref(0);
 
@@ -33,6 +35,15 @@
 
       </div>
     </header>
+
+    <main v-if="presupuesto > 0">
+      <div class="crear-gasto">
+        <img
+        :src="iconoNuevogasto"
+        alt="Icono nuevo gasto"
+        >
+      </div>
+    </main>
 
   </div>
 </template>
@@ -103,4 +114,16 @@
     border-radius: 1.2rem;
     padding: 5rem;
   }
+
+  .crear-gasto {
+    position: fixed;
+    bottom: 5rem;
+    right: 5rem;
+  }
+  
+  .crear-gasto img {
+    width: 5rem;
+    cursor: pointer;
+  }
+
 </style>
