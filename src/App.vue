9c144526d361ab1cd4b406a92dd2407634ca5e1e
Anderson Sánchez <anderson2002y@gmail.com>
2025-04-03 10:04:37 -0600

    feat(expenses): calculate total spent and available balance

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { ref, reactive } from 'vue';
+  import { ref, reactive, watch } from 'vue';
   import Presupuesto from './components/Presupuesto.vue';
   import ControlPresupuesto from './components/ControlPresupuesto.vue';
   import Modal from './components/Modal.vue';
@@ -14,6 +14,7 @@
 
   const presupuesto = ref(0);
   const disponible = ref(0);
+  const gastado = ref(0);
 
   const gasto = reactive({
     nombre: '',
@@ -24,6 +25,14 @@
   });
 
   const gastos = ref([]);
+
+  watch(gastos, () => {
+    const totalGastado = gastos.value.reduce((total, gasto) => gasto.cantidad + total, 0);
+    gastado.value = totalGastado;
+    disponible.value = presupuesto.value - gastado.value;
+  }, {
+    deep:true
+  });
 
   const definirPresupuesto = (cantidad) => {
     presupuesto.value = cantidad;
@@ -85,6 +94,7 @@
           v-else
           :presupuesto="presupuesto"
           :disponible="disponible"
+          :gastado="gastado"
         />
 
       </div>
--- src/components/ControlPresupuesto.vue
+++ src/components/ControlPresupuesto.vue
@@ -10,6 +10,10 @@
     disponible: {
       type: Number,
       required: true
+    }, 
+    gastado: {
+      type: Number,
+      required: true
     }
   });
 </script>
@@ -40,7 +44,7 @@
 
       <p>
         <span> Gastado: </span>
-        $0
+        {{ formatearCantidad(gastado) }}
       </p>
 
     </div>
