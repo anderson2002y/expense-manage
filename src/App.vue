7380646ea5be8e864ef2ec7317496cb3bfb61706
Anderson Sánchez <anderson2002y@gmail.com>
2025-04-03 08:09:45 -0600

    feat(expenses): generate unique ID for expenses

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
   import Presupuesto from './components/Presupuesto.vue';
   import ControlPresupuesto from './components/ControlPresupuesto.vue';
   import Modal from './components/Modal.vue';
+  import { generarId } from './helpers'
   import iconoNuevogasto from './assets/img/nuevo-gasto.svg';
 
   const modal = reactive({
@@ -49,7 +50,7 @@
   const guardarGasto = () => {
     gastos.value.push({
       ...gasto,
-      id:123
+      id: generarId()
     });
   }
 
