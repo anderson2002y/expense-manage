94b6cdd727781cbdabfe5f6c6876b1b6c482ea3a
Anderson Sánchez <anderson2002y@gmail.com>
2025-04-04 17:06:53 -0600

    feat(ui): start circle progress with vue3-circle-progress to display expenses percentage

--- src/components/ControlPresupuesto.vue
+++ src/components/ControlPresupuesto.vue
@@ -1,5 +1,6 @@
 <script setup>
-  import imagen from '../assets/img/grafico.jpg'
+  import CircleProgrees from 'vue3-circle-progress';
+  import "vue3-circle-progress/dist/circle-progress.css"
   import { formatearCantidad } from '../helpers';
 
   defineEmits(['reset-app']);
@@ -23,8 +24,13 @@
 <template>
   <div class="dos-columnas">
     <div class="contenedor-grafico">
-      <img
-        :src="imagen"
+      <CircleProgrees
+        :percent="50"
+        :size="250"
+        :border-width="30"
+        :border-bg-width="30"
+        fill-color="#3b82f6"
+        empty-color="#e1e1e1"
       />
     </div>
 
