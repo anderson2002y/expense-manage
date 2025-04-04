0c4fc2549189b8c2d0f1f83aead26d01b6a5b670
Anderson Sánchez <anderson2002y@gmail.com>
2025-04-04 17:35:13 -0600

    feat(ui): make circle progress dynamic with real-time expense data

--- src/components/ControlPresupuesto.vue
+++ src/components/ControlPresupuesto.vue
@@ -1,4 +1,5 @@
 <script setup>
+  import { computed } from 'vue';
   import CircleProgrees from 'vue3-circle-progress';
   import "vue3-circle-progress/dist/circle-progress.css"
   import { formatearCantidad } from '../helpers';
@@ -19,13 +20,19 @@
       required: true
     }
   });
+
+  const porcentaje = computed(() => {
+    return parseInt(((props.presupuesto - props.disponible) / props.presupuesto) * 100);
+  });
 </script>
 
 <template>
   <div class="dos-columnas">
     <div class="contenedor-grafico">
+      <p class="porcentaje"> {{ porcentaje }}%</p>
+
       <CircleProgrees
-        :percent="50"
+        :percent="porcentaje"
         :size="250"
         :border-width="30"
         :border-bg-width="30"
@@ -62,6 +69,23 @@
 </template>
 
 <style scoped>
+
+  .contenedor-grafico {
+    position: relative;
+  }
+
+  .porcentaje {
+    position: absolute;
+    margin: auto;
+    top: calc(50% - 1.5rem);
+    left: 0;
+    right: 0;
+    text-align: center;
+    z-index: 100;
+    font-size: 3rem;
+    font-weight: 900;
+    color: var(--gris-oscuro);
+  }
 
   .dos-columnas {
     display: flex;
