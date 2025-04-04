b8d4833adc071ed5b62f2f04f4899c9835f270ab
Anderson Sánchez <anderson2002y@gmail.com>
2025-04-04 16:12:45 -0600

    feat(expenses): filter expenses by category

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
-  import { ref, reactive, watch } from 'vue';
+  import { ref, reactive, watch, computed } from 'vue';
   import Presupuesto from './components/Presupuesto.vue';
   import ControlPresupuesto from './components/ControlPresupuesto.vue';
   import Modal from './components/Modal.vue';
   import Gasto from './components/Gasto.vue';
+  import Filtro from './components/Filtro.vue';
   import { generarId } from './helpers'
   import iconoNuevogasto from './assets/img/nuevo-gasto.svg';
 
@@ -15,6 +16,7 @@
   const presupuesto = ref(0);
   const disponible = ref(0);
   const gastado = ref(0);
+  const filtro = ref('');
 
   const gasto = reactive({
     nombre: '',
@@ -108,6 +110,13 @@
     }
   }
 
+  const gastosFiltrados =computed(() => {
+    if(filtro.value) {
+      return gastos.value.filter(gasto => gasto.categoria === filtro.value);
+    }
+    return gastos.value;
+  });
+
 </script>
 
 <template>
@@ -133,13 +142,17 @@
       </div>
     </header>
 
+    <Filtro
+      v-model:filtro ="filtro"
+    />
+
     <main v-if="presupuesto > 0">
 
       <div class="listado-gastos contenedor">
-        <h2> {{ gastos.length > 0 ? 'Gastos' : 'No Hay Gastos'}}</h2>
+        <h2> {{ gastosFiltrados.length > 0 ? 'Gastos' : 'No Hay Gastos'}}</h2>
 
         <Gasto
-          v-for="gasto in gastos"
+          v-for="gasto in gastosFiltrados"
           :key="gasto.id"
           :gasto="gasto"
           @seleccionar-gasto="seleccionarGasto"
